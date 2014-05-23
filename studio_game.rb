81f46d85a487dbdf4be13b969f512aa333193232
Patrick Jones <pmichaeljones@gmail.com>
2014-05-23 13:42:37 -0600

    larry curly moe

---
+++ studio_game.rb
@@ -0,0 +1,9 @@
+player1 = "larry"
+player2 = "curly"
+player3 = "moe"
+
+player1_health = 60
+
+puts "#{player1}'s health is #{player1_health}"
+
+puts "Players:\n\t#{player1}\n\t#{player2}\n\t#{player3}"

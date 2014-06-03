e66e814e565f2c0837e2d71158134e91380fecbc
Patrick Jones <pmichaeljones@gmail.com>
2014-06-03 11:57:55 -0600

    added methods

--- studio_game.rb
+++ studio_game.rb
@@ -1,23 +1,11 @@
-player1 = "larry"
-health1 = 60
+def say_hello(name, health=100)
+  "I'm #{name.capitalize} with a health of #{health}."
+end
 
-player2 = "curly"
-health2 = 125
+puts say_hello("larry", 60)
+puts say_hello("curly", 125)
+puts say_hello("moe")
+puts say_hello("shemp", 90)
 
-player3 = "moe"
-health3 = 100
-
-player4 = "shemp"
-health4 = 90
-
-puts "#{player1.capitalize}'s health is #{health1}"
-
-puts "#{player2.upcase}'s health is #{health2}"
-
-puts "#{player3.capitalize} has a health of #{health3}".center(50,'*')
-
-puts "#{player4.capitalize.ljust(25,'.')}#{health4} health"
-
-current_time = Time.new
 
 puts "The game started at #{current_time.strftime('%A %m/%d/%y at %I:%M%p')}"
